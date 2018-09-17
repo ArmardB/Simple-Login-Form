ef55ee5c6f7a5a89a9ee53f68cdccc803b3a7926
Armard Bellamy <armardbellamy@gmail.com>
2018-09-17 19:48:42 -0400

    Added username and password text field

--- lib/ui/Login.dart
+++ lib/ui/Login.dart
@@ -11,12 +11,55 @@ class Login extends StatefulWidget {
 }
 
 class LoginState extends State<Login>{
+
+  final TextEditingController _userController = new TextEditingController();
+  final TextEditingController _passwordController = new TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Login"),
+        centerTitle: true,
         backgroundColor: Colors.blueAccent,
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: new Container(
+        alignment: Alignment.topCenter,
+        child: new Column(
+          children: <Widget>[
+            new Image.asset(
+              'images/face.png',
+               width: 90.0,
+               height: 90.0,
+              color: Colors.white70,
+            ),
+            new Container(
+              height: 180.0,
+              width: 380.0,
+              color: Colors.white,
+              child: new Column(
+                children: <Widget>[
+                  new TextField(
+                    controller: _userController,
+                    decoration: new InputDecoration(
+                      hintText: 'Username',
+                      icon: new Icon(Icons.person)
+                    ),
+                  ),
+                  new TextField(
+                    controller: _passwordController,
+                    decoration:  new InputDecoration(
+                      hintText: 'Password',
+                      icon: new Icon(Icons.lock)
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
