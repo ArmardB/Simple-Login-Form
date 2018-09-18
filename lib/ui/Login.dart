d05d83ee511011d264c3d18f2d586faa8cbac495
Armard Bellamy <armardbellamy@gmail.com>
2018-09-17 20:28:38 -0400

    Add buttons and footer label

--- lib/ui/Login.dart
+++ lib/ui/Login.dart
@@ -15,6 +15,8 @@ class LoginState extends State<Login>{
   final TextEditingController _userController = new TextEditingController();
   final TextEditingController _passwordController = new TextEditingController();
 
+  
+
 
   @override
   Widget build(BuildContext context) {
@@ -54,12 +56,56 @@ class LoginState extends State<Login>{
                       hintText: 'Password',
                       icon: new Icon(Icons.lock)
                     ),
-                  )
+                  ),
+                  new Padding(padding: new EdgeInsets.all(10.5)),
+                  new Center(
+                    child: new Row(
+                      children: <Widget>[
+                        new Container(
+                          margin: const EdgeInsets.only(left: 38.0),
+                          child:  new RaisedButton(onPressed: () => debugPrint('Login'),
+                            color: Colors.redAccent,
+                              child: new Text("LOGIN",
+                                style: new TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.9,
+                              )),
+                          ),
+                        ),
+                        new Container(
+                          margin: const EdgeInsets.only(left: 120.0),
+                          child:  new RaisedButton(onPressed: () => debugPrint('RESET'),
+                            color: Colors.redAccent,
+                            child: new Text("RESET",
+                                style: new TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.9,
+                                )),
+                          ),
+                        ),
+                      ],
+                    )
+                  ),
                 ],
               ),
-            )
+            ),
+            new Padding(padding: new EdgeInsets.all(14.0)),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text("Welcome, Armard",
+                    style: new TextStyle(
+                        color: Colors.white,
+                        fontSize: 19.4,
+                        fontWeight: FontWeight.w500,
+                    )
+                )
+              ],
+            ),
           ],
+
         ),
+
       ),
     );
   }
